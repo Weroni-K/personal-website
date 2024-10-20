c2cfabcecf72700a637edfe4324b2ba8ffe400fa
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-10-20 22:43:56 +0300

    index and main upload

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/main.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+const app = createApp(App)
+app.use(router)
+app.use(AOS.init())
+app.mount('#app')
