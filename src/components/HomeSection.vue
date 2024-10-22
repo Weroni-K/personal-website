05eb65bf5d1ca556fbfc88d1423e1c3808cef9b5
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-10-22 23:07:27 +0300

    HomeSection added

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home">
+    <TextAnimation />
+    <div class="title-small"><h1>Welcome!</h1></div>
+    <h2>
+      My name is <span class="primary"><b>Weronika</b></span
+      >, I am a junior <span class="primary"><b>Full Stack Developer</b></span> based in
+      <span class="primary"><b>Helsinki</b></span
+      >, <span class="primary"><b>Finland</b></span
+      >. Welcome to my website.
+    </h2>
+  </div>
+</template>
+
+<script setup>
+import TextAnimation from './home-components/TextAnimation.vue'
+</script>
+
+<style scoped>
+#home {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20vh;
+  margin-left: 160px;
+  margin-right: 320px;
+}
+.title-small {
+  display: none;
+}
+</style>
